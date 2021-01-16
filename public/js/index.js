56c2503d10f0683e5158888e44233027dbed0a4b
Gonda Bálint <balinteus@gmail.com>
2021-01-16 22:33:06 +0100

    improve index page

---
+++ public/js/index.js
@@ -0,0 +1,47 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    message: "",
+    error: "",
+    error_code: "",
+    url: "",
+    slug: "",
+    prev_shorten: [],
+    seen: false,
+  },
+  methods: {
+    shorten: async function () {
+      try {
+        const response = await fetch("/api/shorten", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ url: this.url, slug: this.slug }),
+        });
+        if (!response.ok) {
+          this.error = response.statusText;
+          this.error_code = response.status;
+          response.text().then((result) => {
+            this.message = result;
+          });
+        } else {
+          response.json().then((result) => {
+            // Clearing errors
+            this.error = "";
+            this.eror_code = "";
+            // Clearing inputs
+            this.url = "";
+            this.slug = "";
+            // Saving result
+            this.seen = true;
+            this.prev_shorten = [result["_id"], result["url"], result["slug"]];
+            this.message = result["slug"];
+          });
+        }
+      } catch (error) {
+        this.message = error.message;
+      }
+    },
+  },
+});
